056bceeaab7e202e801f37615fb8cb04f11420b3
Carver Harrison <pcarverb@hotmail.com>
2017-12-11 08:24:00 -0800

    Update wiki.js

--- wiki.js
+++ wiki.js
@@ -101,7 +101,8 @@ function domUpdateAuthor(author) {
 
 function updatePage() {
   require(['lib/mustache/mustache.js'], function(Mustache) {
-    document.getElementById("layout").innerHTML = Mustache.render(document.getElementById("tp-page").innerHTML, pagedata);
+    document.getElementById('layout').innerHTML = Mustache.render(document.getElementById('tp-page').innerHTML, pagedata);
+    console.log('Page updated')
   });
 }
 
