f967c9a4318072efed00871e0c729ffa31ffa503
Carver Harrison <pcarverb@hotmail.com>
2017-12-11 17:38:03 -0800

    Update wiki.js

--- wiki.js
+++ wiki.js
@@ -175,7 +175,7 @@ var specialPages = {
       */
 
       jget('FILES', function(data) {
-        var afiles = data.split('\n').shift();
+        var afiles = data.split('\n');
         var files = [];
         afiles.forEach(function(file) {
           if (RegExp(t.toLowerCase()).test(file.toLowerCase())) {
@@ -184,8 +184,7 @@ var specialPages = {
         });
         sresults.innerHTML = Mustache.render(document.getElementById('tp-results').innerHTML, {
           query: t,
-          results: files,
-          updated: new Date(data.split('\n')[0].format("dd.mm.yyyy hh:MM:ss"))
+          results: files
         });
       });
     }
